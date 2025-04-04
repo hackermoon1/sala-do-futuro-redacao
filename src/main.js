2acf9ee7be75e2d2bd43ff15a4de8649b1359656
hackermoon <sorasavio@gmail.com>
2025-04-04 18:25:37 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const config = {
     GEMINI_API_BASE: 'https://generativelanguage.googleapis.com/v1beta/models/',
     GEMINI_MODEL: 'gemini-pro:generateContent',
     API_KEY: 'AIzaSyBhli8mGA1-1ZrFYD1FZzMFkHhDrdYCXwY',
-    UI_SCRIPT_URL: 'https://res.cloudinary.com/dctxcezsd/raw/upload/v1743499848/ui.js',
+    UI_SCRIPT_URL: 'https://res.cloudinary.com/dctxcezsd/raw/upload/v1743801918/menu.js',
     TEMPERATURE: 0.7
 };
 
